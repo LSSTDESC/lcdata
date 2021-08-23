cbeaa68b80593c5d500bb64fa5bf8389bbab83b8
Kyle Boone <kboone@berkeley.edu>
2021-08-23 15:26:37 -0700

    Add test for warn_first_time

--- lcdata/tests/test_utils.py
+++ lcdata/tests/test_utils.py
@@ -6,3 +6,15 @@ def test_generate_object_id():
     object_id_2 = lcdata.utils.generate_object_id()
 
     assert object_id_1 != object_id_2
+
+
+def test_warn_first_time(capsys):
+    lcdata.utils.warn_first_time('key1', 'Warning 1')
+    captured = capsys.readouterr()
+    assert 'Warning 1' in captured.err
+    lcdata.utils.warn_first_time('key1', 'Warning 2')
+    captured = capsys.readouterr()
+    assert captured.err == ''
+    lcdata.utils.warn_first_time('key2', 'Warning 3')
+    captured = capsys.readouterr()
+    assert 'Warning 3' in captured.err
