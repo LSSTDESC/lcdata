8968c8744a114e50df7b45a0e9e63f5d0dd2ad0b
Kyle Boone <kboone@berkeley.edu>
2021-09-13 17:10:11 -0700

    Move generate_object_id to lightcurve.py

--- lcdata/tests/test_lightcurve.py
+++ lcdata/tests/test_lightcurve.py
@@ -7,3 +7,10 @@ def test_light_curve_schema():
 
 def test_light_curve_meta_schema():
     lcdata.schema.verify_schema(lcdata.lightcurve.light_curve_meta_schema)
+
+
+def test_generate_object_id():
+    object_id_1 = lcdata.lightcurve.generate_object_id()
+    object_id_2 = lcdata.lightcurve.generate_object_id()
+
+    assert object_id_1 != object_id_2
--- lcdata/tests/test_utils.py
+++ lcdata/tests/test_utils.py
@@ -1,13 +1,6 @@
 import lcdata
 
 
-def test_generate_object_id():
-    object_id_1 = lcdata.utils.generate_object_id()
-    object_id_2 = lcdata.utils.generate_object_id()
-
-    assert object_id_1 != object_id_2
-
-
 def test_warn_first_time(capsys):
     lcdata.utils.warn_first_time('key1', 'Warning 1')
     captured = capsys.readouterr()
